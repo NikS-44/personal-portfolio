df24834e06dbd8026e32f1f1fa6618538138a94c
Nik Shah <nikps@amazon.com>
2024-10-20 18:56:53 -0600

    feat: improve responsiveness and add linkedin

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import GitHubIcon from "@/app/components/GithubIcon";
+import LinkedInIcon from "@/app/components/LinkedInIcon";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-dvh font-[family-name:var(--font-geist-sans)]">
       <header className="bg-white shadow-md sticky top-0 z-50 w-full">
-        <div className="container mx-auto flex justify-between items-center p-4">
-          <div className="text-2xl font-bold text-gray-800">MyPortfolio</div>
-          <nav className="flex gap-2 space-x-6">
+        <div className="mx-auto flex justify-between items-center p-4 pt-1 flex-wrap max-w-7xl">
+          <nav className="flex gap-2 space-x-6 pt-3 mr-6">
             <a href="#about" className="text-gray-700 hover:text-blue-600 transition-colors duration-300">
               About
             </a>
@@ -20,7 +20,7 @@ export default function Home() {
               Contact
             </a>
             <a
-              className="flex items-center gap-2 hover:underline hover:underline-offset-4 text-gray-700 hover:text-blue-600 transition-colors duration-300"
+              className="flex items-center gap-2 hover:underline hover:underline-offset-4 text-gray-700 hover:text-blue-600 transition-colors duration-300 text-nowrap"
               href="https://github.com/NikS-44/personal-portfolio"
               target="_blank"
               rel="noopener noreferrer"
@@ -29,6 +29,17 @@ export default function Home() {
               Source Code
             </a>
           </nav>
+          <div className="font-bold text-gray-800 pt-3">
+            <a
+              className="flex items-center gap-2 hover:underline hover:underline-offset-4 text-gray-700 hover:text-blue-600 transition-colors duration-300 text-nowrap"
+              href="https://www.linkedin.com/in/nik-shah-657ba616/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <LinkedInIcon />
+              Hire Me!
+            </a>
+          </div>
         </div>
       </header>
       <main className="flex-grow w-full">
