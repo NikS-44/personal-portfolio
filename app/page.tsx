9cec5671dd71aba62d525d4639e68e32834cd40b
Nik Shah <nikps@amazon.com>
2024-10-20 19:06:36 -0600

    feat: update page structure

--- app/page.tsx
+++ app/page.tsx
@@ -4,40 +4,40 @@ import LinkedInIcon from "@/app/components/LinkedInIcon";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-dvh font-[family-name:var(--font-geist-sans)]">
-      <header className="bg-white shadow-md sticky top-0 z-50 w-full">
+      <header className="bg-black text-white shadow-md sticky top-0 z-50 w-full">
         <div className="mx-auto flex justify-between items-center p-4 pt-1 flex-wrap max-w-7xl">
-          <nav className="flex gap-2 space-x-6 pt-3 mr-6">
-            <a href="#about" className="text-gray-700 hover:text-blue-600 transition-colors duration-300">
+          <nav className="font-bold flex gap-2 space-x-6 pt-3 mr-6">
+            <a href="#about" className="hover:text-cyan-400 transition-colors duration-300">
               About
             </a>
-            <a href="#projects" className="text-gray-700 hover:text-blue-600 transition-colors duration-300">
+            <a href="#projects" className="hover:text-cyan-400 transition-colors duration-300">
               Projects
             </a>
-            <a href="#skills" className="text-gray-700 hover:text-blue-600 transition-colors duration-300">
+            <a href="#skills" className="hover:text-cyan-400 transition-colors duration-300">
               Skills
             </a>
-            <a href="#contact" className="text-gray-700 hover:text-blue-600 transition-colors duration-300">
+            <a href="#contact" className="hover:text-cyan-400 transition-colors duration-300">
               Contact
             </a>
-            <a
-              className="flex items-center gap-2 hover:underline hover:underline-offset-4 text-gray-700 hover:text-blue-600 transition-colors duration-300 text-nowrap"
-              href="https://github.com/NikS-44/personal-portfolio"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <GitHubIcon />
-              Source Code
-            </a>
           </nav>
-          <div className="font-bold text-gray-800 pt-3">
+          <div className="font-bold pt-3 flex">
             <a
-              className="flex items-center gap-2 hover:underline hover:underline-offset-4 text-gray-700 hover:text-blue-600 transition-colors duration-300 text-nowrap"
+              className="flex items-center gap-2 hover:underline hover:underline-offset-4 hover:text-cyan-400 transition-colors duration-300 text-nowrap mr-3 pr-3 border-r-2 border-gray-300"
               href="https://www.linkedin.com/in/nik-shah-657ba616/"
               target="_blank"
               rel="noopener noreferrer"
             >
               <LinkedInIcon />
               Hire Me!
+            </a>
+            <a
+              className="flex items-center gap-2 hover:underline hover:underline-offset-4 hover:text-cyan-400 transition-colors duration-300 text-nowrap"
+              href="https://github.com/NikS-44/personal-portfolio"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <GitHubIcon />
+              Source Code
             </a>
           </div>
         </div>
