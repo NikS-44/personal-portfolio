d6da7621a5ccac408c91d09b8b3e2cc49b341c0f
Nik Shah <nikshahee@gmail.com>
2024-11-24 23:02:49 -0700

    feat: styling updates

--- app/page.tsx
+++ app/page.tsx
@@ -7,33 +7,34 @@ import React from "react";
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col font-[family-name:var(--font-geist-sans)]">
-      <header className="fixed top-0 z-50 w-full bg-black text-white shadow-md">
-        <div className="mx-auto flex flex-wrap items-center justify-between p-4 pt-1">
-          <nav className="mr-6 flex flex-grow justify-center gap-2 space-x-6 text-nowrap pt-3 font-bold">
-            <a href="#about" className="transition-colors duration-300 hover:text-cyan-400">
-              About Me
-            </a>
-            <a href="#resume" className="transition-colors duration-300 hover:text-cyan-400">
-              Resume
-            </a>
-            <a href="#projects" className="transition-colors duration-300 hover:text-cyan-400">
-              Projects
-            </a>
-            <a
-              className="mr-2 flex items-center gap-2 border-gray-300 transition-colors duration-300 hover:text-cyan-400 hover:underline hover:underline-offset-4"
-              href="https://github.com/NikS-44/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <GitHubIcon />
-              <span className="hidden sm:inline">Github</span>
-            </a>
-          </nav>
-        </div>
+      <header className="fixed z-50 flex w-full justify-center bg-black py-3 text-white shadow-md">
+        <nav className="flex w-fit items-center gap-2 space-x-6 text-nowrap rounded-lg bg-neutral-900 px-4 py-3">
+          <a href="#about" className="transition-colors duration-300 hover:text-cyan-400">
+            About Me
+          </a>
+          <a href="#resume" className="transition-colors duration-300 hover:text-cyan-400">
+            Resume
+          </a>
+          <a href="#projects" className="transition-colors duration-300 hover:text-cyan-400">
+            Projects
+          </a>
+          <a
+            className="mr-2 flex items-center gap-2 border-gray-300 transition-colors duration-300 hover:text-cyan-400 hover:underline hover:underline-offset-4"
+            href="https://github.com/NikS-44/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <GitHubIcon />
+            <span className="hidden sm:inline">GitHub</span>
+          </a>
+        </nav>
       </header>
       <main className="w-full flex-grow bg-black">
-        <section id="about" className="mx-auto mb-4 flex max-w-2xl flex-col items-center gap-6 px-8 pt-20 text-white">
-          <h1 className="text-3xl">Hi, I&apos;m Nik!</h1>
+        <section
+          id="about"
+          className="mx-auto mb-4 flex min-h-svh max-w-2xl flex-col items-center justify-center gap-6 px-8 pt-20 text-white"
+        >
+          <h1 className="text-6xl">Hi, I&apos;m Nik!</h1>
           <p className="mt-5">
             I&#39;m a front-end developer with a passion for all things tech and love the challenge of solving complex
             problems. In my free time, you can find me at the climbing gym, in the mountains, or setting up the latest
@@ -49,27 +50,32 @@ export default function Home() {
             it is paramount to build inclusive and usable websites for all.
           </p>
           <div className="flex gap-4">
-            <a
-              className="flex items-center gap-2 text-nowrap rounded bg-black px-4 py-2 text-white outline outline-white transition-colors duration-300 hover:text-cyan-200"
-              href="#resume"
-            >
-              My Work
-            </a>
-            <a
-              className="flex items-center gap-2 text-nowrap rounded bg-black px-4 py-2 text-white outline outline-cyan-200 transition-colors duration-300 hover:text-cyan-200"
-              href="https://www.linkedin.com/in/nik-shah-657ba616/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <LinkedInIcon />
-              Hire Me!
-            </a>
+            <div className="relative rounded-lg border-2 border-transparent text-white transition-colors hover:border-neutral-500">
+              <a
+                className="relative flex animate-[moving-cyan-black_3s_linear_infinite] items-center gap-2 text-nowrap rounded-md bg-gradient-to-r from-cyan-900 to-black bg-[length:200%_200%] px-4 py-2 transition-colors duration-300"
+                href="https://www.linkedin.com/in/nik-shah-657ba616/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <LinkedInIcon />
+                Hire Me!
+              </a>
+            </div>
+            <div className="relative rounded-lg border-2 border-transparent text-white transition-colors hover:border-cyan-600">
+              <a
+                className="relative flex animate-[moving-cyan-black_3s_linear_infinite] items-center gap-2 text-nowrap rounded-md bg-gradient-to-r from-cyan-900 to-black bg-[length:200%_200%] px-4 py-2 transition-colors duration-300"
+                href="#resume"
+                rel="noopener noreferrer"
+              >
+                My Work
+              </a>
+            </div>
           </div>
         </section>
-        <section id="resume" className="px-2.5 pb-16 pt-16">
+        <section id="resume" className="min-h-svh px-2.5 pb-16 pt-16">
           <Resume />
         </section>
-        <section id="projects" className="flex flex-col items-center justify-start py-16">
+        <section id="projects" className="flex min-h-svh flex-col items-center justify-start py-16">
           <Projects />
         </section>
       </main>
