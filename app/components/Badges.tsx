49e7e8ee7ded32fda33e18b8d5a14905a64279d5
Nik Shah <nikshahee@gmail.com>
2024-11-28 15:10:42 -0700

    feat: make badges more responsive

--- app/components/Badges.tsx
+++ app/components/Badges.tsx
@@ -14,9 +14,9 @@ const Badges = ({ badges }: BadgesProps) => {
     <section className={"mt-4 flex flex-wrap gap-2"}>
       {badges.map(({ text, icon }) => {
         return (
-          <div key={text} className={"rounded px-2 py-1 outline outline-1 outline-cyan-200"}>
+          <div key={text} className={"flex items-center rounded px-2 py-1 outline outline-1 outline-cyan-200"}>
             {icon}
-            <span>{text}</span>
+            <span className="text-[clamp(0.625rem,0.425rem_+_1vw,1rem)]">{text}</span>
           </div>
         );
       })}
